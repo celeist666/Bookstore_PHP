6fe2e6b8ced3451fc20d7591797b1766ae7a27b8
celeist666 <athebate@gmail.com>
2020-04-08 21:27:16 +0900

    Create practice.php

---
+++ practice.php
@@ -0,0 +1,10 @@
+<?php
+try {
+$pdo = new PDO('mysql:host=localhost;dbname=ijdb',
+'idjbuser', ’mypassword’);
+$output = 'Database connection established.';
+} 
+catch (PDOException $e) {
+$output = 'Unable to connect to the database server.';
+}
+ ?>
